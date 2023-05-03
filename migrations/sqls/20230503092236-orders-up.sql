d2d4fd3e8a3479c532f5da3b1de84ba081e989d8
J-Mann-123 <jmann5326@gmail.com>
2023-05-03 18:56:12 +0930

    added code to new migrations

--- migrations/sqls/20230503092236-orders-up.sql
+++ migrations/sqls/20230503092236-orders-up.sql
@@ -1,1 +1,10 @@
-/* Replace with your SQL commands */
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER,
+    quantity INTEGER,
+    user_id INTEGER,
+    status BOOLEAN,
+
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
--- migrations/sqls/20230503092318-users-up.sql
+++ migrations/sqls/20230503092318-users-up.sql
@@ -1,1 +1,6 @@
-/* Replace with your SQL commands */
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL
+);
