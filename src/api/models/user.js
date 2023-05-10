d13f31442f35ad126ac552c937ce6f2ae9b0d24e
J-Mann-123 <jmann5326@gmail.com>
2023-05-10 19:25:08 +0930

    Made updates to user file

--- src/api/models/user.js
+++ src/api/models/user.js
@@ -7,17 +7,63 @@ exports.Users = void 0;
 // @ts-ignore
 const database_1 = __importDefault(require("../database"));
 class Users {
+    // index CRUD Method (Reads)
     async index() {
         try {
             // @ts-ignore
             const conn = await database_1.default.connect();
             const sql = 'SELECT * FROM users';
             const result = await conn.query(sql);
-            conn.rekease();
+            conn.release();
             return result.rows;
         }
         catch (err) {
             throw new Error(`Cannot get users ${err}`);
+        }
+    }
+    // show CRUD Method (Updates)
+    async show(id) {
+        try {
+            const sql = 'SELECT * FROM users WHERE id=($1)';
+            // @ts-ignore
+            const conn = await database_1.default.connect();
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+        }
+        catch (err) {
+            throw new Error(`Could not find user ${id}. Error: ${err}`);
+        }
+    }
+    // create CRUD Method (Creates)
+    async create(u) {
+        try {
+            const sql = 'INSERT INTO users (first_name, last_name, password) VALUES($1, $2, $3, $4) RETURNING *';
+            // @ts-ignore
+            const conn = await database_1.default.connect();
+            const result = await conn
+                .query(sql, [u.firstName, u.lastName, u.password]);
+            const user = result.rows[0];
+            conn.release();
+            return user;
+        }
+        catch (err) {
+            throw new Error(`Could not add new user ${u.firstName} ${u.lastName}. Error: ${err}`);
+        }
+    }
+    // delete CRUD Method (Deletes)
+    async delete(id) {
+        try {
+            const sql = 'DELETE FROM users WHERE id=($1)';
+            // @ts-ignore
+            const conn = await database_1.default.connect();
+            const result = await conn.query(sql, [id]);
+            const user = result.rows[0];
+            conn.release();
+            return user;
+        }
+        catch (err) {
+            throw new Error(`Could not delete user ${id}.  Error: ${err}`);
         }
     }
 }
--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -9,16 +9,59 @@ export type User = {
 }
 
 export class Users {
+    // index CRUD Method (Reads)
     async index (): Promise<User[]> {
         try {
             // @ts-ignore
             const conn = await Client.connect()
             const sql = 'SELECT * FROM users'
             const result = await conn.query(sql)
-            conn.rekease()
+            conn.release()
             return result.rows
         } catch (err) {
             throw new Error(`Cannot get users ${err}`)
+        }
+    }
+    // show CRUD Method (Updates)
+    async show (id: string): Promise<User> {
+        try {
+            const sql = 'SELECT * FROM users WHERE id=($1)'
+            // @ts-ignore
+            const conn = await Client.connect()
+            const result = await conn.query(sql, [id])
+            conn.release()
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Could not find user ${id}. Error: ${err}`)
+        }
+    }
+    // create CRUD Method (Creates)
+    async create (u: User): Promise<User> {
+        try {
+            const sql = 'INSERT INTO users (first_name, last_name, password) VALUES($1, $2, $3, $4) RETURNING *'
+            // @ts-ignore
+            const conn = await Client.connect()
+            const result = await conn
+                .query(sql, [u.firstName, u.lastName, u.password])
+            const user = result.rows[0]
+            conn.release()
+            return user
+        } catch (err) {
+            throw new Error(`Could not add new user ${u.firstName} ${u.lastName}. Error: ${err}`)
+        }
+    }
+    // delete CRUD Method (Deletes)
+    async delete (id: string): Promise<User> {
+        try {
+            const sql = 'DELETE FROM users WHERE id=($1)'
+            // @ts-ignore
+            const conn = await Client.connect()
+            const result = await conn.query(sql, [id])
+            const user = result.rows[0]
+            conn.release()
+            return user
+        } catch (err) {
+            throw new Error(`Could not delete user ${id}.  Error: ${err}`)
         }
     }
 }
