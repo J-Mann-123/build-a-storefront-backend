ca2a9e84dadd7edf121170f2f86e361b57975f3b
J-Mann-123 <jmann5326@gmail.com>
2023-05-31 15:15:13 +0930

    Added user authentication func

--- src/api/handlers/user.ts
+++ src/api/handlers/user.ts
@@ -30,6 +30,26 @@ const create = async (req: Request, res: Response) => {
     } catch (err) {
         res.status(400)
         res.json(err)
+        return
+    }
+}
+
+const authenticate = async (req: Request, res: Response) => {
+    try {
+        const user: User = {
+            id: req.body.id,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: req.body.password,
+        }
+        const tokenSecret: string = process.env.TOKEN_SECRET as string;
+        const u = await store.authenticate(user.firstName, user.lastName, user.password)
+        var token = jwt.sign({ user: u }, tokenSecret);
+        res.json(token)
+    } catch (err) {
+        res.status(401)
+        res.json(err)
+        return
     }
 }
 
@@ -44,6 +64,7 @@ const userRoutes = (app: express.Application) => {
     app.get('/user/:id', show)
     app.post('/newUser', create)
     app.delete('/deleted/:id', destroy)
+    app.get('/user/:id', authenticate)
 }
 
 export default userRoutes
