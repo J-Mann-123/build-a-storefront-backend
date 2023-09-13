fe36546a9c9e8ae790f1c5a55a4e095e76853b3c
J-Mann-123 <jmann5326@gmail.com>
2023-09-13 20:18:50 +0930

    Added endpoint testing on product

--- src/spec/tests/product_spec.ts
+++ src/spec/tests/product_spec.ts
@@ -1,4 +1,4 @@
-import { AllProducts } from '../../api/models/product';
+import { AllProducts, Product } from '../../api/models/product';
 
 const store = new AllProducts()
 
@@ -15,4 +15,14 @@ describe("All Users", () => {
     it("should have product delete method", () => {
         expect(store.delete).toBeDefined();
     });
+    it("should return the list of the products", async () => {
+        const productList: Product[] = await AllProducts.index();
+        expect(productList).toEqual([
+            {
+                id: 1,
+                name: 'bucket',
+                price: 12,
+            }
+        ])
+    })
 })
